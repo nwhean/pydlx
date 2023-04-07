a63b785a0b93e092ab5bb1df4d5d640b053868c7
Wee Hean Ng <heanconcerto@gmail.com>
2023-04-07 08:45:04 +0800

    Renamed package to pydlx

--- test_dlx.py
+++
@@ -1,8 +0,0 @@
-"""Test file for the dlx package"""
-import doctest
-import dlx.operations
-import dlx.link
-
-MODULES = (dlx.operations, dlx.link)
-for mod in MODULES:
-    doctest.testmod(dlx.operations)
